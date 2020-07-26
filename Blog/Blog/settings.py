00ecbaa8cab1b0a65f436fb92d41c67728d02290
Evan-Pi <vaggelispissinas@gmail.com>
2020-07-27 00:27:52 +0300

    maria db

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -118,10 +118,23 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'evangelo912107_phoenixfeather_db',
+        'USER': 'evangelo912107_phoenixfeather_user',
+        'PASSWORD': '#ep7776PHI!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
