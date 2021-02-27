c0b9aa3ce0afda15ea418d996c4400f17a7341bc
Evan <vaggelispissinas@gmail.com>
2021-02-28 00:26:32 +0200

    installed apps - removed quill

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'colorfield',
     'admin_interface',
 
-    'django_quill',
     # Default apps
     'django.contrib.admin',
     
@@ -177,21 +176,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_REDIRECT_URL = 'login_success'
-
-
-
-TINYMCE_DEFAULT_CONFIG = {
-    "height": "520px",
-    "width": "100%",
-    "menubar": "file edit view insert format tools table help",
-    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
-    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
-    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
-    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
-    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
-    "a11ycheck ltr rtl | showcomments addcomment code",
-    "custom_undo_redo_levels": 10,
-    "language": "en_US",  # To force a specific language instead of the Django current language.
-}
-
